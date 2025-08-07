93f1ce1da12fbe88c8a269b56f56a77f018cac60
Jassu123197 <jsangam330@gmail.com>
2025-08-07 07:29:30 +0530

    Delete monocart_order/monocart_order directory

--- monocart_order/monocart_order/src/main/java/com/monocartOrder/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monocartOrder.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.monocartOrder.model.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
--- monocart_order/monocart_order/src/main/java/com/monocartOrder/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.monocartOrder.repository;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.monocartOrder.model.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {}
-
